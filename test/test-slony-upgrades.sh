f07ad72ed357a4113a2ff61e7c943e19959ba2c8
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2018-11-06 18:23:18 -0500

    Integrating better version_log logic into Slony code, refactoring the Slony test to be a series of Bash functions

--- test/test-slony-upgrades.sh
+++ test/test-slony-upgrades.sh
@@ -99,29 +99,34 @@ slonydir=$TARGETMHDIR/slony
 mkdir -p ${slonydir}/pid
 
 kill_all_slons
-# Drop databases and then create them
-for node in origin rep2 rep3 rep4 rep5 rep6; do
-    glog user.info "Drop and recreate database ${node} on cluster ${clusterdb}"
-    psql -d ${clusterdb} \
-	 -c "drop database if exists ${node};"
-    psql -d ${clusterdb} \
-         -c "create database ${node};"
-    # Create a wee table as an initialization point
-    psql -d ${DBCLUSTER}/${node} \
-	 -c "create schema nullschema;  create table nullschema.wee_table(id serial primary key, data text);"
-done
 
-slonikpreamble=${slonydir}/preamble.slonik
-echo "
+function drop_and_recreate_databases () {
+    # Drop databases and then create them
+    for node in origin rep2 rep3 rep4 rep5 rep6; do
+	glog user.info "Drop and recreate database ${node} on cluster ${clusterdb}"
+	psql -d ${clusterdb} \
+	     -c "drop database if exists ${node};"
+	psql -d ${clusterdb} \
+             -c "create database ${node};"
+	# Create a wee table as an initialization point
+	# So Slony as something to replicate from the very beginning
+	psql -d ${DBCLUSTER}/${node} \
+	     -c "create schema nullschema; create table nullschema.wee_table(id serial primary key, data text);"
+    done
+}
+
+function set_up_preamble () {
+    slonikpreamble=${slonydir}/preamble.slonik
+    echo "
 cluster name=${SLONYCLUSTER};
 " > $slonikpreamble
 
-for nodeinfo in origin:1 rep2:2 rep3:3 rep4:4: rep5:5 rep6:6; do
-    nodenum=`echo $nodeinfo | cut -d : -f 2` 
-    nodedb=`echo $nodeinfo | cut -d : -f 1`
-    conffile=${slonydir}/slon.conf.${nodenum}
-    glog user.info "Set up slon.conf for node $nodenum - ${conffile}"
-    echo "### Slony conf for node ${nodenum}
+    for nodeinfo in origin:1 rep2:2 rep3:3 rep4:4: rep5:5 rep6:6; do
+	nodenum=`echo $nodeinfo | cut -d : -f 2` 
+	nodedb=`echo $nodeinfo | cut -d : -f 1`
+	conffile=${slonydir}/slon.conf.${nodenum}
+	glog user.info "Set up slon.conf for node $nodenum - ${conffile}"
+	echo "### Slony conf for node ${nodenum}
 pid_file='${slonydir}/pid/node${nodenum}.pid'
 cluster_name='${SLONYCLUSTER}'
 conn_info='${DBCLUSTER}/${nodedb}'
@@ -129,13 +134,15 @@ syslog=2
 syslog_facility=LOCAL0
 syslog_ident=slon
 " > ${conffile}
-    echo "node $nodenum admin conninfo = '${DBCLUSTER}/${nodedb}';" >> $slonikpreamble
-done
+	echo "node $nodenum admin conninfo = '${DBCLUSTER}/${nodedb}';" >> $slonikpreamble
+    done
+}
 
-# Set up the 6 nodes as a Slony cluster
-initnodes=${slonydir}/init.slonik
-#  Initialize each node
-echo "# Initialize nodes
+function initialize_slony_nodes () {
+    # Set up the 6 nodes as a Slony cluster
+    initnodes=${slonydir}/init.slonik
+    #  Initialize each node
+    echo "# Initialize nodes
 include <${slonikpreamble}>;
 init cluster (id=1, comment='Mahout test node origin');
 store node (id=2, comment='Node for mahout test', event node=1);
@@ -145,40 +152,46 @@ store node (id=5, comment='Node for mahout test', event node=1);
 store node (id=6, comment='Node for mahout test', event node=1);
 " > $initnodes
 
-glog user.notice "Initialize Slony cluster, including creating nodes"
-$PGBINDIR/slonik $initnodes
+    glog user.notice "Initialize Slony cluster, including creating nodes"
+    $PGBINDIR/slonik $initnodes
+}
 
-#  Store paths
-storepaths=${slonydir}/storepaths.slonik
-echo "# Initialize nodes
+function store_paths () {
+    #  Store paths
+    storepaths=${slonydir}/storepaths.slonik
+    echo "# Initialize nodes
 include <${slonikpreamble}>;
 " > $storepaths
-for nodeinfo in origin:1 rep2:2 rep3:3 rep4:4: rep5:5 rep6:6; do
-    nodenum=`echo $nodeinfo | cut -d : -f 2` 
-    nodedb=`echo $nodeinfo | cut -d : -f 1` 
-    for nodeinfo2 in origin:1 rep2:2 rep3:3 rep4:4: rep5:5 rep6:6; do
-	nodenum2=`echo $nodeinfo2 | cut -d : -f 2` 
-	nodedb2=`echo $nodeinfo2 | cut -d : -f 1` 
-	if [ $nodenum -ne $nodenum2 ]; then
-	    echo "store path (server=$nodenum2, client=$nodenum, conninfo='${DBCLUSTER}/${nodedb2}');" >> $storepaths
-	else
-	    echo "Omit self-talk - $nodenum $nodenum2"
-	fi
+    for nodeinfo in origin:1 rep2:2 rep3:3 rep4:4: rep5:5 rep6:6; do
+	nodenum=`echo $nodeinfo | cut -d : -f 2` 
+	nodedb=`echo $nodeinfo | cut -d : -f 1` 
+	for nodeinfo2 in origin:1 rep2:2 rep3:3 rep4:4: rep5:5 rep6:6; do
+	    nodenum2=`echo $nodeinfo2 | cut -d : -f 2` 
+	    nodedb2=`echo $nodeinfo2 | cut -d : -f 1` 
+	    if [ $nodenum -ne $nodenum2 ]; then
+		echo "store path (server=$nodenum2, client=$nodenum, conninfo='${DBCLUSTER}/${nodedb2}');" >> $storepaths
+	    else
+		echo "Omit self-talk - $nodenum $nodenum2"
+	    fi
+	done
     done
-done
 
-glog user.notice "Store connection paths for Slony"
-$PGBINDIR/slonik $storepaths
+    glog user.notice "Store connection paths for Slony"
+    $PGBINDIR/slonik $storepaths
+}
 
-# Launch slons
-for i in 1 2 3 4 5 6; do
-    glog user.notice "Launch slon daemon for node $i"
-    $PGBINDIR/slon -f ${slonydir}/slon.conf.${i} &
-done
+function launch_slons () {
+    # Launch slons
+    for i in 1 2 3 4 5 6; do
+	glog user.notice "Launch slon daemon for node $i"
+	$PGBINDIR/slon -f ${slonydir}/slon.conf.${i} &
+    done
+}
 
-# Subscribe set for set 1, with the one table, nullschema.wee_table
-initset=$slonydir/initset.slonik
-echo "# Initialize nodes
+function subscribe_initial_set () {
+    # Subscribe set for set 1, with the one table, nullschema.wee_table
+    initset=$slonydir/initset.slonik
+    echo "# Initialize nodes
 include <${slonikpreamble}>;
 create set (id=${SLONYMAINSET}, origin=1, comment='${SLONYCLUSTER} Tables and Sequences');
 set add table (id=0, set id=${SLONYMAINSET}, origin=1, fully qualified name='nullschema.wee_table', comment='basic table');
@@ -194,79 +207,86 @@ subscribe set (id=${SLONYMAINSET}, provider=4, receiver=6, forward=yes);
 wait for event (origin=all, confirmed=all, wait on=1);
 " > $initset
 
-glog user.notice "Subscribe all nodes to simple set"
-$PGBINDIR/slonik $initset
+    glog user.notice "Subscribe all nodes to simple set"
+    $PGBINDIR/slonik $initset
+}
 
-# Set up a schema in devdb, and use mahout to extract buildable schemas
-devdb=devdb
-devuri=${DBCLUSTER}/${devdb}
-comparisondb=comparisondb
-compuri=${DBCLUSTER}/${comparisondb}
+function initial_mahout_schema () {
+    # Set up a schema in devdb, and use mahout to extract buildable schemas
+    devdb=devdb
+    devuri=${DBCLUSTER}/${devdb}
+    comparisondb=comparisondb
+    compuri=${DBCLUSTER}/${comparisondb}
 
-for i in ${devdb} ${comparisondb}; do
-    glog user.info "Drop and recreate database ${i} on cluster ${clusterdb}"
-    psql -d ${clusterdb} \
-	 -c "drop database if exists ${i};"
-    psql -d ${clusterdb} \
-         -c "create database ${i};"
-done
+    for i in ${devdb} ${comparisondb}; do
+	glog user.info "Drop and recreate database ${i} on cluster ${clusterdb}"
+	psql -d ${clusterdb} \
+	     -c "drop database if exists ${i};"
+	psql -d ${clusterdb} \
+             -c "create database ${i};"
+    done
 
-glog user.notice "Purge away ./${PROJECTNAME}"
-if [ -d ./${PROJECTNAME} ]; then
-    rm -rf ./${PROJECTNAME}
-else
-    if [ -e ./${PROJECTNAME} ]; then
-	glog user.error "projectname directory [./${PROJECTNAME}] exists and is not a directory"
-	exit 2
-    fi
-fi
-
-glog user.notice "Check availability of pgcmp"
-if [ -d ${PGCMPHOME} ]; then
-    if [ -x ${PGCMPHOME}/pgcmp ]; then
-		glog user.notice "Ready to run pgcmp as ${PGCMPHOME}/pgcmp"
+    glog user.notice "Purge away ./${PROJECTNAME}"
+    if [ -d ./${PROJECTNAME} ]; then
+	rm -rf ./${PROJECTNAME}
     else
-		glog user.error "pgcmp not executable as ${PGCMPHOME}/pgcmp"
-		exit 1
+	if [ -e ./${PROJECTNAME} ]; then
+	    glog user.error "projectname directory [./${PROJECTNAME}] exists and is not a directory"
+	    exit 2
+	fi
     fi
-else
-    glog user.error "No such directory: ${PGCMPHOME}"
-    exit 1
-fi
 
-glog user.notice "Check availability of mahout"
-if [ -d ${MAHOUTHOME} ]; then
-    if [ -x ${MAHOUTHOME}/mahout ]; then
-		glog user.notice "Ready to run mahout as ${MAHOUTHOME}/mahout"
+    glog user.notice "Check availability of pgcmp"
+    if [ -d ${PGCMPHOME} ]; then
+	if [ -x ${PGCMPHOME}/pgcmp ]; then
+	    glog user.notice "Ready to run pgcmp as ${PGCMPHOME}/pgcmp"
+	else
+	    glog user.error "pgcmp not executable as ${PGCMPHOME}/pgcmp"
+	    exit 1
+	fi
     else
-		glog user.error "mahout not executable as ${MAHOUTHOME}/mahout"
-		exit 1
+	glog user.error "No such directory: ${PGCMPHOME}"
+	exit 1
     fi
-else
-    glog user.error "No such directory: ${MAHOUTHOME}"
-    exit 1
-fi
 
-glog user.notice "Do mahout init to capture the empty base schema"
+    glog user.notice "Check availability of mahout"
+    if [ -d ${MAHOUTHOME} ]; then
+	if [ -x ${MAHOUTHOME}/mahout ]; then
+	    glog user.notice "Ready to run mahout as ${MAHOUTHOME}/mahout"
+	else
+	    glog user.error "mahout not executable as ${MAHOUTHOME}/mahout"
+	    exit 1
+	fi
+    else
+	glog user.error "No such directory: ${MAHOUTHOME}"
+	exit 1
+    fi
 
-MAHOUTSCHEMA=MaHoutSchema PGCMPHOME=${PGCMPHOME} MAINDATABASE=${devuri} SUPERUSERACCESS=${SUPERCLUSTER}/${devdb} COMPARISONDATABASE=${compuri} ${MAHOUT} init ${PROJECTNAME}
+    glog user.notice "Do mahout init to capture the empty base schema"
 
-glog user.notice "Do mahout capture without introducing any changes; expect no change"
-# Run mahout capture, without any change
-(cd ${PROJECTNAME}; ${MAHOUT} capture)
+    MAHOUTSCHEMA=MaHoutSchema PGCMPHOME=${PGCMPHOME} MAINDATABASE=${devuri} SUPERUSERACCESS=${SUPERCLUSTER}/${devdb} COMPARISONDATABASE=${compuri} ${MAHOUT} init ${PROJECTNAME}
 
-glog user.notice "Add a common tests section"
-mkdir ${PROJECTNAME}/common-tests
-echo "
+}
+
+function capture_null () {
+    glog user.notice "Do mahout capture without introducing any changes; expect no change"
+    # Run mahout capture, without any change
+    (cd ${PROJECTNAME}; ${MAHOUT} capture)
+}
+
+function common_tests () {
+    glog user.notice "Add a common tests section"
+    mkdir ${PROJECTNAME}/common-tests
+    echo "
 
 common tests
 " >> ${PROJECTNAME}/mahout.control
 
-echo "select 1;" > ${PROJECTNAME}/common-tests/null-test.sql
-echo "  psqltest from 1.1 to 1.3 common-tests/null-test.sql" >> ${PROJECTNAME}/mahout.control
+    echo "select 1;" > ${PROJECTNAME}/common-tests/null-test.sql
+    echo "  psqltest from 1.1 to 1.3 common-tests/null-test.sql" >> ${PROJECTNAME}/mahout.control
 
-echo "  psqltest from Base common-tests/pk-test.sql" >> ${PROJECTNAME}/mahout.control
-echo "
+    echo "  psqltest from Base common-tests/pk-test.sql" >> ${PROJECTNAME}/mahout.control
+    echo "
 do \$\$
 declare
    prec record;
@@ -284,8 +304,8 @@ end
 \$\$ language plpgsql;
 " > ${PROJECTNAME}/common-tests/pk-test.sql
 
-echo "  psqltest common-tests/multiply-defined.sql" >> ${PROJECTNAME}/mahout.control
-echo "
+    echo "  psqltest common-tests/multiply-defined.sql" >> ${PROJECTNAME}/mahout.control
+    echo "
 do \$\$
 declare
   c_found boolean;
@@ -308,19 +328,21 @@ end
 \$\$ language plpgsql;
 
 " > ${PROJECTNAME}/common-tests/multiply-defined.sql
+}
 
-
-glog user.notice "Set up filesystem to use the captured mahout config for a fresh install"
-# Set up target directory for sample installation
-rm -rf ${TARGETDIR}
-mkdir ${TARGETDIR}
-cp -r ${PROJECTNAME} ${TARGETDIR}
+function install_target () {
+    glog user.notice "Set up filesystem to use the captured mahout config for a fresh install"
+    # Set up target directory for sample installation
+    rm -rf ${TARGETDIR}
+    mkdir ${TARGETDIR}
+    cp -r ${PROJECTNAME} ${TARGETDIR}
+}
 
 function fix_install_uri () {
     # Drop alternate configuration into installation Mahout config
     local installuri
     installuri=${DBCLUSTER}/origin
-    glog user.info "fix up install instance to use ${installuri} rather than the URI provided in the build"
+    glog user.info "Change install instance to use ${installuri} rather than the URI provided in the build"
     egrep -v MAINDATABASE ${TARGETMHDIR}/mahout.conf | \
     egrep -v SUPERUSERACCESS ${TARGETMHDIR}/mahout.conf \
 	> ${TARGETMHDIR}/mahout.conf.keep
@@ -337,14 +359,16 @@ MAHOUTOMITSCHEMAS=\"('_${SLONYCLUSTER}', 'nullschema', 'pg_catalog', 'informatio
 " >> ${TARGETMHDIR}/mahout.conf.keep
     cp ${TARGETMHDIR}/mahout.conf.keep ${TARGETMHDIR}/mahout.conf
 }
-fix_install_uri
 
-glog user.notice "Attach base schema to Slony replicas"
-(cd ${TARGETMHDIR}; ${MAHOUT} slony_attach Base)
+function attach_base_to_replicas () {
+    glog user.notice "Attach base schema to Slony replicas"
+    (cd ${TARGETMHDIR}; ${MAHOUT} slony_attach Base)
+}
 
-glog user.notice "Prepare upgrade for v1.1"
-# Create a couple of upgrades
-echo "
+function prep_v11 () {
+    glog user.notice "Prepare upgrade for v1.1"
+    # Create a couple of upgrades
+    echo "
 
 version 1.1
 requires Base
@@ -352,8 +376,8 @@ ddl 1.1/stuff.sql
 
 " >> ${PROJECTNAME}/mahout.control
 
-mkdir ${PROJECTNAME}/1.1
-echo "
+    mkdir ${PROJECTNAME}/1.1
+    echo "
    create table if not exists t1 (id serial primary key, name text not null unique, created_on timestamptz default now());
    create schema if not exists subschema;
    create table if not exists subschema.t2 (id serial primary key, name text not null unique, created_on timestamptz default now());
@@ -361,25 +385,31 @@ echo "
    create schema if not exists \"StudlySchema\";
    create table if not exists \"StudlySchema\".\"StudlyTable\" (id serial primary key, name text not null unique);
 " > ${PROJECTNAME}/1.1/stuff.sql
+} 
 
-glog user.notice "mahout capture on v1.1"
-(cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.1 tar.gz)
+function capture_v11 () {
+    glog user.notice "mahout capture on v1.1"
+    (cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.1 tar.gz)
+}
 
-glog user.notice "do upgrade of the install instance to run v1.1"
-cp -r ${PROJECTNAME} ${TARGETDIR}
-fix_install_uri
-# And try to install the upgrade
-(cd ${TARGETMHDIR}; ${MAHOUT} slonik; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.1.slonik)
+function install_v11_on_cluster () {
+    glog user.notice "do upgrade of the install instance to run v1.1"
+    cp -r ${PROJECTNAME} ${TARGETDIR}
+    fix_install_uri
+    # And try to install the upgrade
+    (cd ${TARGETMHDIR}; ${MAHOUT} slonik; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.1.slonik)
+}
 
-echo "
+function prep_v12 () {
+    echo "
 
 version 1.2
 requires 1.1
 ddl 1.2/stuff.sql
 
 " >> ${PROJECTNAME}/mahout.control
-mkdir ${PROJECTNAME}/1.2
-echo "
+    mkdir ${PROJECTNAME}/1.2
+    echo "
    create table t3 (
      id serial primary key,
      name text not null unique,
@@ -387,18 +417,23 @@ echo "
 
 " > ${PROJECTNAME}/1.2/stuff.sql
 
-glog user.notice "Run mahout capture to put that into the build"
+    glog user.notice "Run mahout capture to put that into the build"
+}
 
-glog user.notice "mahout capture on v1.2"
-(cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.2 tar.gz)
+function capture_v12 () {
+    glog user.notice "mahout capture on v1.2"
+    (cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.2 tar.gz)
+}
 
-glog user.notice "do upgrade of the install instance to run v1.2"
-cp -r ${PROJECTNAME} ${TARGETDIR}
-fix_install_uri
+function install_v12_on_cluster () {
+    glog user.notice "do upgrade of the install instance to run v1.2"
+    cp -r ${PROJECTNAME} ${TARGETDIR}
+    fix_install_uri
+    (cd ${TARGETMHDIR}; ${MAHOUT} slonik; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.2.slonik)
+}
 
-(cd ${TARGETMHDIR}; ${MAHOUT} slonik; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.2.slonik)
-
-echo "
+function prep_v13 () {
+    echo "
 
 version 1.3
 requires 1.2
@@ -406,18 +441,22 @@ ddl 1.3/stuff.sql
 
 " >> ${PROJECTNAME}/mahout.control
 
-mkdir ${PROJECTNAME}/1.3
+    mkdir ${PROJECTNAME}/1.3
 
-echo "
+    echo "
    alter table t3 add column deleted_on timestamptz;
    create index t3_deleted on t3(deleted_on) where (deleted_on is not null);
 
 " > ${PROJECTNAME}/1.3/stuff.sql
+}
 
-glog user.notice "mahout capture on v1.3"
-(cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.3 tar.gz; ${MAHOUT} slonik ; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.3.slonik)
+function capture_v13 () {
+    glog user.notice "mahout capture on v1.3"
+    (cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.3 tar.gz; ${MAHOUT} slonik ; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.3.slonik)
+}
 
-echo "
+function prep_v14 () {
+    echo "
 
 version 1.4
 requires 1.3
@@ -425,26 +464,56 @@ ddl 1.4/stuff.sql
 
 " >> ${PROJECTNAME}/mahout.control
 
-echo "
+    echo "
 select 1/0;
 " > ${PROJECTNAME}/common-tests/failing-test.sql
 
-mkdir ${PROJECTNAME}/1.4
+    mkdir ${PROJECTNAME}/1.4
 
-echo "
+    echo "
    alter table t3 drop column deleted_on;
    alter table t3 add column updated_on timestamptz default now();
 " > ${PROJECTNAME}/1.4/stuff.sql
+}
 
-glog user.notice "mahout capture on v1.4"
-(cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.4 tar.gz)
+function capture_v14 () {
+    glog user.notice "mahout capture on v1.4"
+    (cd ${PROJECTNAME}; ${MAHOUT} capture; ${MAHOUT} build ${PROJECTNAME}-v1.4 tar.gz)
+}
 
-glog user.notice "do upgrade of the install instance to run v1.3, v1.4"
-cp -r ${PROJECTNAME} ${TARGETDIR}
-fix_install_uri
-(cd ${TARGETMHDIR}; ${MAHOUT} slonik; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.4.slonik))
-
-glog user.notice "Completed upgrade to v1.4"
-
-# At the end, kill off slons
+function install_v14_on_cluster () {
+    glog user.notice "do upgrade of the install instance to run v1.3, v1.4"
+    cp -r ${PROJECTNAME} ${TARGETDIR}
+    fix_install_uri
+    (cd ${TARGETMHDIR}; ${MAHOUT} slonik; ${PGBINDIR}/slonik .mahout-temp/mahout-ddl-script-1.4.slonik)
+    glog user.notice "Completed upgrade to v1.4"
+}
+ 
+# Start...
 kill_all_slons
+drop_and_recreate_databases
+set_up_preamble
+initialize_slony_nodes
+store_paths
+launch_slons
+subscribe_initial_set
+initial_mahout_schema
+capture_null
+common_tests
+install_target
+fix_install_uri
+attach_base_to_replicas
+prep_v11
+capture_v11
+install_v11_on_cluster
+prep_v12
+capture_v12
+install_v12_on_cluster
+prep_v13
+capture_v13
+prep_v14
+capture_v14
+install_v14_on_cluster
+kill_all_slons
+
+
