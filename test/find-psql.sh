0b831e5b8dc22034b7a87734aa7c4c663a527440
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2020-03-16 14:55:37 -0400

    /usr/lib maybe

--- test/find-psql.sh
+++ test/find-psql.sh
@@ -10,4 +10,15 @@ for p in $(echo $PATH | sed 's/:/ /g'); do
     fi
 done
 
+
+for p in /usr/lib/postgresql/11/bin /usr/lib/postgresql/12/bin ; do
+    echo "Check $p"
+    if [ -d $p ]; then
+	echo "$p exists as a directory"
+	if [ -x $p/psql ]; then
+	    echo "$p/psql is executable"
+	fi
+    fi
+done
+
 which psql
