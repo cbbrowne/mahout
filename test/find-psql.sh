774c839a9d486a50ef11294f93a46ae210c363fd
cbbrowne <cbbrowne@cbbrowne.tor.int-afilias.info>
2020-03-16 14:47:41 -0400

    Search properly for psql

---
+++ test/find-psql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for p in $(echo $PATH | sed 's/:/ /g'); do
+    echo "Check $p"
+    if [ -d $p ]; then
+	echo "$p exists as a directory"
+	if [ -x $p/psql ]; then
+	    echo "$p/psql is executable"
+	fi
+    fi
+done
